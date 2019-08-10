0ee4389ff41920bae1d24633624dc201ced7914d
Viktor Justo Vasquez <viktor@vjustov.me>
2019-08-10 20:01:24 +0200

    Adds the FeatureFlags and Flag components.

---
+++ src/index.tsx
@@ -0,0 +1,30 @@
+import React, { ReactChildren, ReactElement } from 'react';
+
+export const FeatureFlagsContext = React.createContext({});
+
+export enum AvailableFlags { }
+// exampleFeatureName = 'exampleFeatureName'
+
+export const FeatureFlags = (props: { children: ReactElement }) => (
+  <FeatureFlagsContext.Provider value={flags}>
+    {props.children}
+  </FeatureFlagsContext.Provider>
+);
+
+interface Prop {
+  name: AvailableFlags;
+  children: ReactChildren | ReactElement;
+}
+export const Flag = (props: Prop) => {
+  return (
+    <FeatureFlagsContext.Consumer>
+      {(flagsObj: { [key: string]: boolean }) =>
+        flagsObj[props.name] ? props.children : null
+      }
+    </FeatureFlagsContext.Consumer>
+  );
+};
+
+const flags = {
+  // feature: true
+};
